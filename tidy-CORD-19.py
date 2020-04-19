498c6e3f8fdfc64966ee9d9000f43ce331cb2ada
Simon Crase <simon@greenweaves.nz>
2020-04-19 18:29:23 +1200

    First step towards linking tables

--- tidy-CORD-19.py
+++ tidy-CORD-19.py
@@ -15,18 +15,36 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
+# cord_uid
+# sha
+# source_x
+# title
+# doi
+# pmcid
+# pubmed_id
+# license
+# abstract
+# publish_time
+# authors
+# journal
+# Microsoft_Academic_Paper_ID
+# WHO_Covidence
+# has_pdf_parse
+# has_pmc_xml_parse
+# full_text_file
+# url
+
+
 import json, os, pandas as pd, re, sys
 from os.path import join
 cord_path = r'C:\CORD-19'
 
-def create_meta_data(name='metadata.csv'):
-    product  = pd.read_csv(join(cord_path,name))
+def create_meta_data(metadata='metadata.csv',cord_path=cord_path):
+    product         = pd.read_csv(join(cord_path,metadata))
     product.columns = [re.sub('[^_a-zA-Z0-9]+','_',col) for col in product.columns]
-    for col in product.columns:
-        print (col)
     return product
 
-def create_json_dict():
+def create_json_dict(cord_path=cord_path):
     product = {}
     for root, _, files in os.walk(cord_path):
         for name in files:
@@ -40,12 +58,35 @@ def create_json_dict():
                         print ('{0} error: {1}'.format(absolute_json_file_path,err))
     return product
 
-if __name__=='__main__':
-    metadata = create_meta_data()
-    papers   = create_json_dict()
-    #metadata.set_index('sha')
+
+    
+def link_data(metadata,papers):
+    matched     =0
+    not_matched = 0
     for paper_sha in papers.keys():
-        matches = metadata.loc[metadata.sha==paper_sha,'title']
-        c=matches.count()
-        print (paper_sha,matches.count())
+        matches_sha = metadata.loc[metadata.sha.contains(paper_sha),'title']
+        if matches_sha.count()==1:
+            matched+=1
+        else:
+            matches_pmcid = metadata.loc[metadata.pmcid==paper_sha,'title']
+            if matches_pmcid.count()==1:
+                matched+=1
+            else:
+                print (paper_sha)
+                not_matched+=1
+    print ('matched={0}, not_matched={1}'.format(matched, not_matched))    
+    
+if __name__=='__main__':
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default=r'C:\CORD-19')
+    parser.add_argument('--metadata', default ='metadata.csv')
+    args = parser.parse_args()
+
+    metadata = create_meta_data(metadata=args.metadata,cord_path=args.path)
+        
+    papers   = create_json_dict(cord_path=args.path)
+    link_data(metadata,papers)
+  
     
