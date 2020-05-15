dea635e2aa8d6697cf36deece86035fe3ada1717
Simon Crase <simon@greenweaves.nz>
2020-05-15 20:37:51 +1200

    Started plotting deaths

--- eliminate.py
+++ eliminate.py
@@ -20,7 +20,7 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import random
 
-def create_NPIs(start=150,R0=2.5,lower_bound=0.9,upper_bound=0.99,dt=5):
+def create_NPIs(start=150,R0=2.5,lower_bound=0.9,upper_bound=1.0,dt=5):
      NPIs = []
      t = start
      while R0>=1.0:
@@ -34,24 +34,24 @@ def evolve(t0,t1,y,R0):
      return solve_ivp(sepir.dy, 
                      (t0,t1),
                      y,
-                     args=(5000, 0.1, 0.25,
+                     args=(5000000, 0.1, 0.25,
                            sepir.get_beta(R0=R0,gamma=0.1,delta=1.0,epsilon=0.15),
                            0.1, 1.0, 0.15, 2/100, 1/100, 300, 1.25/100))
 
-def change_R0(R0=2.5,t_range=(0,400),npis=[(145,0.7),(150,0.8),(155,0.9),(160,0.75)]):
+def change_R0(R0=2.5,t_range=(0,400),NPIs=[(145,0.7),(150,0.8),(155,0.9),(160,0.75)],N=5000000):
      t0 = t_range[0]
-     t1 = npis[0][0]
-     sols = [evolve(t0,t1,[1-20/5000000, 20/5000000, 0, 0, 0, 0, 0],R0=R0)]
+     t1 = NPIs[0][0]
+     sols = [evolve(t0,t1,[1-20/N, 20/N, 0, 0, 0, 0, 0],R0=R0)]
      R0s  = [R0]
-     for i in range(len(npis)-1):
+     for i in range(len(NPIs)-1):
           t0 = t1
-          t1 = npis[i+1][0]
-          R0*= npis[i][1]
+          t1 = NPIs[i+1][0]
+          R0*= NPIs[i][1]
           sols.append(evolve(t0,t1,[y[-1] for y in sols[-1].y],R0=R0))
           R0s.append(R0)
      t0 = t1
      t1 = t_range[1]
-     R0*= npis[len(npis)-1][1]
+     R0*= NPIs[len(NPIs)-1][1]
      sols.append(evolve(t0,t1,[y[-1] for y in sols[-1].y],R0=R0))
      R0s.append(R0)
      return sols,R0s
@@ -59,10 +59,14 @@ def change_R0(R0=2.5,t_range=(0,400),npis=[(145,0.7),(150,0.8),(155,0.9),(160,0.
 
 if __name__=='__main__':
      random.seed(None)
-     for i in range(5):
-          sols,R0s=change_R0(npis=create_NPIs())
+     N=5000000
+     for i in range(25):
+          sols,R0s=change_R0(NPIs=create_NPIs(),N=N)
           plt.figure()
           for sol,R0 in zip(sols,R0s):
-               plt.plot(sol.t,sepir.aggregate(sol.y,selector=range(3,5)),label=f'{R0:.3f}')     
+               plt.plot(sol.t,sepir.scale(sepir.aggregate(sol.y,selector=range(3,5)),N=N),label=f'{R0:.3f}')
+               #print (sepir.scale(sepir.aggregate(sol.y,selector=range(5,7)),N=N))
+          deaths = int((1-sepir.aggregate(sols[-1].y,selector=range(5,7))[-1])*N)
+          plt.title(f'Deaths={deaths}')
           plt.legend(title='R0')
      plt.show()
