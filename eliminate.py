4c0819b0ca3610c68fb27e289599c8fc7dcffbf8
Simon Crase <simon@greenweaves.nz>
2020-05-16 10:36:53 +1200

    Parse command line parameters

--- eliminate.py
+++ eliminate.py
@@ -30,43 +30,114 @@ def create_NPIs(start=150,R0=2.5,lower_bound=0.9,upper_bound=1.0,dt=5):
           t += random.randint(1,dt)
      return NPIs
           
-def evolve(t0,t1,y,R0):
+def evolve(t0,t1,y,
+           R0      = 2.5,
+           N       = 5000000, 
+           c       = 0.1, 
+           alpha   = 0.25,   
+           gamma   = 0.1,  
+           delta   = 1,   
+           epsilon = 0.15, 
+           CFR1    = 2.0/100,  
+           CFR0    = 1.0/100,  
+           nICU    = 300,   
+           pICU    = 1.25/100 ):
      return solve_ivp(sepir.dy, 
                      (t0,t1),
                      y,
-                     args=(5000000, 0.1, 0.25,
-                           sepir.get_beta(R0=R0,gamma=0.1,delta=1.0,epsilon=0.15),
-                           0.1, 1.0, 0.15, 2/100, 1/100, 300, 1.25/100))
+                     args=(N, c, alpha,
+                           sepir.get_beta(R0=R0,gamma=gamma,delta=delta,epsilon=epsilon),
+                           gamma, delta, epsilon, CFR1, CFR0, nICU, pICU))
 
-def change_R0(R0=2.5,t_range=(0,400),NPIs=[(145,0.7),(150,0.8),(155,0.9),(160,0.75)],N=5000000):
+def change_R0(R0      = 2.5,
+              t_range = (0,400),
+              NPIs    = [(145,0.7),(150,0.8),(155,0.9),(160,0.75)],
+              N       = 5000000,
+              initial = 20,
+              c       = 0.1, 
+              alpha   = 0.25,   
+              gamma   = 0.1,  
+              delta   = 1,   
+              epsilon = 0.15, 
+              CFR1    = 2.0/100,  
+              CFR0    = 1.0/100,  
+              nICU    = 300,   
+              pICU    = 1.25/100):
      t0 = t_range[0]
      t1 = NPIs[0][0]
-     sols = [evolve(t0,t1,[1-20/N, 20/N, 0, 0, 0, 0, 0],R0=R0)]
+     sols = [evolve(t0,t1,
+                    [1-initial/N, initial/N, 0, 0, 0, 0, 0],
+                    R0      = R0,
+                    N       = N, 
+                    c       = c, 
+                    alpha   = alpha,   
+                    gamma   = gamma,  
+                    delta   = delta,   
+                    epsilon = epsilon, 
+                    CFR1    = CFR1,  
+                    CFR0    = CFR0,  
+                    nICU    = nICU,   
+                    pICU    = pICU)]
      R0s  = [R0]
      for i in range(len(NPIs)-1):
           t0 = t1
           t1 = NPIs[i+1][0]
           R0*= NPIs[i][1]
-          sols.append(evolve(t0,t1,[y[-1] for y in sols[-1].y],R0=R0))
+          sols.append(evolve(t0,t1,[y[-1] for y in sols[-1].y],R0=R0,gamma=gamma,delta=delta,epsilon=epsilon))
           R0s.append(R0)
      t0 = t1
      t1 = t_range[1]
      R0*= NPIs[len(NPIs)-1][1]
-     sols.append(evolve(t0,t1,[y[-1] for y in sols[-1].y],R0=R0))
+     sols.append(evolve(t0,t1,[y[-1] for y in sols[-1].y],R0=R0,gamma=gamma,delta=delta,epsilon=epsilon))
      R0s.append(R0)
      return sols,R0s
 
 
 if __name__=='__main__':
-     random.seed(None)
-     N=5000000
-     for i in range(25):
-          sols,R0s=change_R0(NPIs=create_NPIs(),N=N)
+     import argparse
+
+     parser = argparse.ArgumentParser('Model COVID19 evolution')
+     parser.add_argument('--M',       type=int,   default = 5,      help='Number of runs for Monte Carlo sumulation')
+     parser.add_argument('--R0',      type=float, default=2.5,      help='Basic Reproduction number')
+     parser.add_argument('--initial', type=int,   default=20,       help='Number of exposed people at start')
+     parser.add_argument('--N',       type=int,   default=5000000,  help='Population size')
+     parser.add_argument('--nICU',    type=int,   default=300,      help='Number of ICU beds')
+     parser.add_argument('--end',     type=int,   default=400,      help='Number of days to be simulated')
+     parser.add_argument('--c',       type=float, default=0.1,      help='testing rate for symptomatic cases, per diem')
+     parser.add_argument('--alpha',   type=float, default=0.25,     help='E to P transition rate, per diem')
+     parser.add_argument('--gamma',   type=float, default=0.1,      help='I to R tranition, per diem')
+     parser.add_argument('--delta',   type=float, default=1.0,      help='P to I, per diem')
+     parser.add_argument('--epsilon', type=float, default=0.15,     help='relative infectiousness')
+     parser.add_argument('--CFR1',    type=float, default=2.0/100,  help='case fatality rate with cases exceeding ICU max')
+     parser.add_argument('--CFR0',    type=float, default=1.0/100,  help='case fatality rate for cases under ICU max')
+     parser.add_argument('--pICU',    type=float, default=1.25/100, help='proportion of cases requiring ICU')
+     parser.add_argument('--show',                default=False,    help='Show plots at end of run', action='store_true')
+     parser.add_argument('--out',                 default='./figs', help='Pathname for output')
+     parser.add_argument('--seed',    type=int,   default=None,     help='Seed for random number generator')
+     args = parser.parse_args()
+     
+     random.seed(args.seed)
+     
+     for i in range(args.M):
+          sols,R0s=change_R0(R0=args.R0,
+                             NPIs    = create_NPIs(),
+                             initial = args.initial,
+                             N       = args.N,
+                             c       = args.c, 
+                             alpha   = args.alpha,   
+                             gamma   = args.gamma,
+                             delta   = args.delta,
+                             epsilon = args.epsilon,
+                             CFR1    = args.CFR1,  
+                             CFR0    = args.CFR0,  
+                             nICU    = args.nICU,   
+                             pICU    = args.pICU)
           plt.figure()
           for sol,R0 in zip(sols,R0s):
-               plt.plot(sol.t,sepir.scale(sepir.aggregate(sol.y,selector=range(3,5)),N=N),label=f'{R0:.3f}')
+               plt.plot(sol.t,sepir.scale(sepir.aggregate(sol.y,selector=range(3,5)),N=args.N),label=f'{R0:.3f}')
                #print (sepir.scale(sepir.aggregate(sol.y,selector=range(5,7)),N=N))
-          deaths = int((1-sepir.aggregate(sols[-1].y,selector=range(5,7))[-1])*N)
-          plt.title(f'Deaths={deaths}')
+          deaths = int((1-sepir.aggregate(sols[-1].y,selector=range(5,7))[-1])*args.N)
+          plt.title(f'Deaths={deaths:,}')
           plt.legend(title='R0')
-     plt.show()
+     if args.show:
+          plt.show()
